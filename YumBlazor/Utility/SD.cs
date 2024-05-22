477127fac83d81168c5bf61635be489e345821a7
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-22 10:08:51 -0500

    validation in action

--- YumBlazor/Utility/SD.cs
+++ YumBlazor/Utility/SD.cs
@@ -4,5 +4,11 @@
     {
         public static string Role_Admin = "Admin";
         public static string Role_Customer = "Customer";
-    }
+		public static string StatusPending = "Pending";
+		public static string StatusApproved = "Approved";
+		public static string StatusReadyForPickUp = "ReadyForPickUp";
+		public static string StatusCompleted = "Completed";
+		public static string StatusCancelled = "Cancelled";
+		public static string StatusRefunded = "Refunded";
+	}
 }
